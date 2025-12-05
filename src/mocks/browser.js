4dcbea24b261cd20b17cd42e2e973d4de446dd42
Yasir349913 <yasirmaqsood534@gmail.com>
2025-12-05 12:22:35 +0500

    added authhandlers in mock service worker browser.js

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,5 +1,14 @@
 import { setupWorker } from "msw/browser"
+import { authHandlers } from "./handlers/authHandlers"
 
-// We'll add handlers as we build features
-// For now, empty array
-export const worker = setupWorker()
+// Combine all handlers as we add more features
+// For now, we only have auth handlers
+const handlers = [
+  ...authHandlers,
+  // Future handlers will be added here:
+  // ...orderHandlers,
+  // ...inventoryHandlers,
+  // etc.
+]
+
+export const worker = setupWorker(...handlers)
