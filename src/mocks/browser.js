08a720ed7efd0683ab3d11b61e97872124aab980
Yasir349913 <yasirmaqsood534@gmail.com>
2025-12-18 18:17:38 +0500

    integrated newmeasurementchartshandler into mockService worker for registration

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,10 +1,11 @@
 import { setupWorker } from "msw/browser"
 import { authHandlers } from "./handlers/authHandlers"
-
+import { measurementChartsHandlers } from "./handlers/measurementChartsHandlers"
 // Combine all handlers as we add more features
 // For now, we only have auth handlers
 const handlers = [
   ...authHandlers,
+  ...measurementChartsHandlers,
   // Future handlers will be added here:
   // ...orderHandlers,
   // ...inventoryHandlers,
