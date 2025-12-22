27861c788988e3bb1e542ec26ab5b478ff791c2e
Yasir349913 <yasirmaqsood534@gmail.com>
2025-12-22 17:50:59 +0500

    integrated inventory Handlers into mock Service Worker for registration

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,11 +1,13 @@
 import { setupWorker } from "msw/browser"
 import { authHandlers } from "./handlers/authHandlers"
 import { measurementChartsHandlers } from "./handlers/measurementChartsHandlers"
+import { inventoryHandlers } from "./handlers/inventoryHandlers"
 // Combine all handlers as we add more features
 // For now, we only have auth handlers
 const handlers = [
   ...authHandlers,
   ...measurementChartsHandlers,
+  ...inventoryHandlers,
   // Future handlers will be added here:
   // ...orderHandlers,
   // ...inventoryHandlers,
