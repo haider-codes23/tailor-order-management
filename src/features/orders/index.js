572487c3ad96ecd676fda265e2638744cba47465
haider-codes23 <haide.codes23@gmail.com>
2025-12-30 09:47:24 +0500

    updated barrel file

---
+++ src/features/orders/index.js
@@ -0,0 +1,14 @@
+/**
+ * Orders Feature - Barrel Export
+ */
+
+// Pages
+export { default as OrdersListPage } from "./pages/OrdersListPage"
+export { default as OrderDetailPage } from "./pages/OrderDetailPage"
+export { default as OrderItemDetailPage } from "./pages/OrderItemDetailPage"
+export { default as OrderFormGeneratorPage } from "./pages/OrderFormGeneratorPage"
+export { default as CreateOrderPage } from "./pages/CreateOrderPage"
+export { default as EditOrderPage } from "./pages/EditOrderPage"
+
+// Components
+export { default as OrderFormPreview } from "./components/OrderFormPreview"
