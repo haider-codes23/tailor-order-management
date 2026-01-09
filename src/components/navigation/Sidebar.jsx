efa0ecf2fe38369fd129883208a15772fe050626
haider-codes23 <haider.code23@gmail.com>
2026-01-09 13:04:58 +0500

    added a new ball to the navbar

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -150,8 +150,8 @@ export default function Sidebar() {
                       group flex items-center px-3 py-2 text-sm font-medium rounded-md transition-colors
                       ${
                         active
-                          ? "bg-slate-100 text-slate-900"
-                          : "text-slate-600 hover:bg-slate-50 hover:text-slate-900"
+                          ? "bg-violet-400 text-stone-100"
+                          : "text-slate-800 hover:bg-slate-50 hover:text-slate-900"
                       }
                     `}
                   >
@@ -164,6 +164,9 @@ export default function Sidebar() {
                       <Icon className={`h-4 w-4 transition-colors ${item.iconColor}`} />
                     </div>
                     {item.name}
+                    {active && (
+                    <div className="ml-auto w-2 h-2 rounded-full bg-accent animate-bounce" />
+                  )}
                   </Link>
                 )
               })
