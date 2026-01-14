50e9ec6b2e586b4c8d811fcd33cddf64ea113cdd
haider-codes23 <haider.code23@gmail.com>
2026-01-14 10:27:52 +0500

    registered procurement handler

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -6,6 +6,7 @@ import { productsHandlers } from "./handlers/productsHandlers"
 import { usersHandlers } from "./handlers/usersHandlers"
 import { ordersHandlers } from "./handlers/ordersHandlers"
 import { fabricationHandlers } from "./handlers/fabricationHandlers"
+import { procurementHandlers } from "./handlers/procurementHandlers"
 // Combine all handlers as we add more features
 // For now, we only have auth handlers
 const handlers = [
@@ -16,6 +17,7 @@ const handlers = [
   ...usersHandlers,
   ...ordersHandlers,
   ...fabricationHandlers,
+  ...procurementHandlers,
   // Future handlers will be added here:
   // ...orderHandlers,
   // ...inventoryHandlers,
